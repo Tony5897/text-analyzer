3180d4d38e56bb49e01e4d665f76f838b8bb1b1a
tonym <tony.martinez5897@gmail.com>
2022-03-29 11:23:31 -0700

    add testing blocks for tdd.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,37 @@
 // Business Logic ----------
 
+// wordCounter() function for brevity
 
-
-
-
-
-
-
-
-
+function numberOfOccurrencesInText(word, text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+  }
+});
+  return wordCount;
+}
 
 
 // User Interface Logic --------------
+
 $(document).ready(function() {
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCounter++;
+    }
+  });
+  return wordCount;
+}
 
 
-
-});

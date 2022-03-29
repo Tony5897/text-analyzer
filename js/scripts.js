ba78b06b057d06f922ddc42492ecbd8ea54fa4ff
tonym <tony.martinez5897@gmail.com>
2022-03-29 14:20:16 -0700

    add further UI logic for refactoring.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
-// Business Logic ----------
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
+// Business Logic
 
 function wordCounter(text) {
   if (text.trim().length === 0) {
@@ -15,7 +21,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -29,6 +35,25 @@ function numberOfOccurrencesInText(word, text) {
 }
 
 // UI Logic
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
  
 $(document).ready(function(){
   $("form#word-counter").submit(function(event){
@@ -39,5 +64,6 @@ $(document).ready(function(){
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });

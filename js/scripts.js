7169a7bc89d7bc3447fc3fcf9159507ac4166093
tonym <tony.martinez5897@gmail.com>
2022-03-29 12:40:43 -0700

    add separation of business and UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 // Business Logic ----------
 
-// wordCounter() function for brevity
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
 
 function numberOfOccurrencesInText(word, text) {
   if (text.trim().length === 0) {
@@ -11,27 +23,21 @@ function numberOfOccurrencesInText(word, text) {
   wordArray.forEach(function(element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
       wordCount++;
-  }
-});
-  return wordCount;
-}
-
-
-// User Interface Logic --------------
-
-$(document).ready(function() {
-function wordCounter(text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  let wordCount = 0;
-  const wordArray = text.split(" ");
-  wordArray.forEach(function(element) {
-    if (!Number(element)) {
-      wordCounter++;
     }
   });
   return wordCount;
 }
 
-
+// UI Logic
+ 
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+  });
+});
